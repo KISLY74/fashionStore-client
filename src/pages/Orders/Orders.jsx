4489d26f9066ef2d3340a4222a9adf8b991523a4
KISLY74 <kisly.nikita@mail.ru>
2023-06-18 12:57:10 +0300

    f

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -58,56 +58,58 @@ const Orders = () => {
           status={status}
           setStatus={setStatus} />
         {isLoading ?
-          <table className="table-orders">
-            <thead>
-              <tr>
-                <th>Номер</th>
-                <th>Создан</th>
-                <th>Цена</th>
-                <th>Статус</th>
-                <th>Товары</th>
-                <th>Доставка</th>
-              </tr>
-            </thead>
-            <tbody className="tbody">
-              {orders && orders.map((e, i) => {
-                return <tr key={JSON.stringify(e)} id="tr">
-                  <td>#{i + 1}</td>
-                  <td>{e.createdAt.slice(0, 10)}</td>
-                  <td>{e.products.reduce((acc, el) => {
-                    return acc + el.product.price * el.count
-                  }, 0)} руб.</td>
-                  <td>{e.status}</td>
-                  <td>
-                    {e.products && e.products.map((item, i) => {
-                      return <div key={JSON.stringify(item)} className="item">
-                        <div className="item-container">
-                          <p className="item-container__name">{item.product.name}</p>
-                          <img className="item-container__img" src={BASE_URL + item.img} alt="dont load" />
-                          <p className="item-container__count">x{item.count}</p>
-                          <p>{item.attributeValues.join(', ')}</p>
-                          <p className="item__price">{item.product.price} руб.</p>
-                        </div>
-                        {e.status === "Завершен" &&
-                          <div className="item__rate">
-                            {new Array(5).fill(0).map((n, j) => {
-                              return <ButtonRate
-                                key={`btn-${j + 1}-${store.user.id}-${item.product.id}`}
-                                userId={store.user.id}
-                                productId={item.product.id}
-                                value={j + 1}
-                                setRating={setRating} />
-                            })}
-                            <p>Поставьте оценку товару!</p>
-                          </div>}
-                      </div>
-                    })}
-                  </td>
-                  <td>{getDateDelivery(e.status)}</td>
+          <div className="container-table">
+            <table className="table-orders">
+              <thead>
+                <tr>
+                  <th>Номер</th>
+                  <th>Создан</th>
+                  <th>Цена</th>
+                  <th>Статус</th>
+                  <th>Товары</th>
+                  <th>Доставка</th>
                 </tr>
-              })}
-            </tbody>
-          </table>
+              </thead>
+              <tbody className="tbody">
+                {orders && orders.map((e, i) => {
+                  return <tr key={JSON.stringify(e)} id="tr">
+                    <td>#{i + 1}</td>
+                    <td>{e.createdAt.slice(0, 10)}</td>
+                    <td>{e.products.reduce((acc, el) => {
+                      return acc + el.product.price * el.count
+                    }, 0)} руб.</td>
+                    <td>{e.status}</td>
+                    <td>
+                      {e.products && e.products.map((item, i) => {
+                        return <div key={JSON.stringify(item)} className="item">
+                          <div className="item-container">
+                            <p className="item-container__name">{item.product.name}</p>
+                            <img className="item-container__img" src={BASE_URL + item.img} alt="dont load" />
+                            <p className="item-container__count">x{item.count}</p>
+                            <p>{item.attributeValues.join(', ')}</p>
+                            <p className="item__price">{item.product.price} руб.</p>
+                          </div>
+                          {e.status === "Завершен" &&
+                            <div className="item__rate">
+                              {new Array(5).fill(0).map((n, j) => {
+                                return <ButtonRate
+                                  key={`btn-${j + 1}-${store.user.id}-${item.product.id}`}
+                                  userId={store.user.id}
+                                  productId={item.product.id}
+                                  value={j + 1}
+                                  setRating={setRating} />
+                              })}
+                              <p>Поставьте оценку товару!</p>
+                            </div>}
+                        </div>
+                      })}
+                    </td>
+                    <td>{getDateDelivery(e.status)}</td>
+                  </tr>
+                })}
+              </tbody>
+            </table>
+          </div>
           : <Spinner />}
       </div>
     </div>
